24f2f5b6d535cdba02dd251eff7d033b6160d58d
'sandeep_jacob' <'sandeepjacob1@gmail.com'>
2023-09-27 15:19:37 +0530

    my first commit

---
+++ src/main/java/com/qashqai/QashqaiAssetProApplication.java
@@ -0,0 +1,15 @@
+package com.qashqai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QashqaiAssetProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QashqaiAssetProApplication.class, args);
+		System.out.println("Server Running ......");
+		System.out.println("hello");
+	}
+
+}
