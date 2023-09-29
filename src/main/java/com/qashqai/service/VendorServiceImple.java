9e7a191776629cc87997f7d1c1bdf006d2bebdcf
AkhilaAjith <akhilaajith321@gmail.com>
2023-09-29 15:36:02 +0530

    "updated controller"

--- src/main/java/com/qashqai/service/VendorServiceImple.java
+++ src/main/java/com/qashqai/service/VendorServiceImple.java
@@ -17,23 +17,25 @@ public class VendorServiceImple implements IVendorService{
 	@Autowired
 	private IVendorRepository vendorRepo;
 
+	//list vendors
 	@Override
 	public List<Vendors> getVendor() {
 		// TODO Auto-generated method stub
 		return (List<Vendors>)vendorRepo.findAll();
 	}
-
+	//add vendors
 	@Override
 	public Vendors saveVendor(Vendors vendor) {
 		// TODO Auto-generated method stub
 	return  vendorRepo.save(vendor);
 	}
-
+	//search vendor
 	@Override
 	public Vendors getVendor(int id) {
 		// TODO Auto-generated method stub
 		return vendorRepo.findById(id).orElseThrow(()->new RuntimeException("Vendor not founds for id "+id));
 	}
+	//disable vender
 	@Transactional
 	@Override
 	public void disableVendor(int id) {
