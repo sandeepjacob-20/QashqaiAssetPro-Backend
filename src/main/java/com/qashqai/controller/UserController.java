74d967afa190ba8d706a970ff703dd464102f20b
'sandeep_jacob' <'sandeepjacob1@gmail.com'>
2023-10-02 23:44:00 +0530

    added cross origin in UsercController

--- src/main/java/com/qashqai/controller/UserController.java
+++ src/main/java/com/qashqai/controller/UserController.java
@@ -2,6 +2,7 @@ package com.qashqai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.qashqai.common.APIResponse;
 import com.qashqai.model.Users;
 import com.qashqai.service.IUserService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UserController {
